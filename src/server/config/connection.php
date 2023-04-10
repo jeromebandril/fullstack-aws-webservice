5d506eb1714b3009f24add5a2cd7d127f8c0af67
jeromebandril <102626866+jeromebandril@users.noreply.github.com>
2023-04-11 01:29:55 +0200

    fix inaccuracies docker-composer and add database credential on php connection

--- src/server/config/connection.php
+++ src/server/config/connection.php
@@ -4,9 +4,9 @@ set_exception_handler(function (Throwable $e) {
   error_log("DATABASE CONNECTION ERROR: $e");
 });
 
-$hostname = "localhost";
-$username = "root";
-$password = "";
+$hostname = "db";
+$username = "admin";
+$password = "admin";
 $database = "jerome_aws_database";
 $port = "3306";
 $conn = new mysqli($hostname, $username, $password, $database, $port);
