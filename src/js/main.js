8ca018d1e9532db04fdce496d2f36d0da03fd789
jeromebandril <102626866+jeromebandril@users.noreply.github.com>
2023-04-11 15:38:02 +0200

    done index.html

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//   // Check for session cookie
+//   if (!document.cookie.includes("session=")) {
+//     // Redirect to login page
+//     console.log("ciao");
+//     window.location.href = "./login.html";
+//   }
+// });
+
+function goToSection(id) {
+  var button = document.querySelector(`button[data-bs-target="#${id}-collapse"]`);
+  var isExpanded = button.getAttribute("aria-expanded");
+  console.log(isExpanded);
+  if (isExpanded === "true") {
+    console.log("vai");
+    location.href = "#" + id;
+  }
+}
